a8b78413b94bd2cca94c690bcdb5988408c54ffd
bondarchukm <bondarchuk.hh@gmail.com>
2021-05-05 21:04:01 +0300

    Types added to userData

--- lib/user.data.ts
+++ lib/user.data.ts
@@ -1,9 +1,9 @@
 export const userData = {
-    standardUserName: 'standard_user',
-    lockedOutUser: 'locked_out_user',
-    problemUser: 'problem_user',
-    perfomanceGlitchUser: 'performance_glitch_user',
-    password: 'secret_sauce',
-    invalidUserName: 'qwerty',
-    invalidPassword: '123123'
+    standardUserName: <string> 'standard_user',
+    lockedOutUser: <string> 'locked_out_user',
+    problemUser: <string> 'problem_user',
+    perfomanceGlitchUser: <string> 'performance_glitch_user',
+    password: <string> 'secret_sauce',
+    invalidUserName: <string> 'qwerty',
+    invalidPassword: <string> '123123',
 }

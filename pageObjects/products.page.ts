74e1c86174a267feb6651ee814eb0628be6d2555
bondarchukm <bondarchuk.hh@gmail.com>
2021-05-06 15:43:32 +0300

    Errors fixed

--- pageObjects/products.page.ts
+++ pageObjects/products.page.ts
@@ -11,19 +11,19 @@ import {
 export class ProductsPageObject {
     pageTitle: ElementFinder
     sortingOptionsDropdown: ElementFinder
-    sortingOptions: ElementFinder
+    sortingOptions: string
 
     constructor() {
         this.pageTitle = $('.title')
         this.sortingOptionsDropdown = $('.product_sort_container')
-        this.sortingOptions = $('.product_sort_container option')
+        this.sortingOptions = '.product_sort_container option'
     }
 
     getPageTitleText(): promise.Promise<string> {
         return this.pageTitle.getText()
     }
-    async getSortingOptionLocator(text: string): Promise<ElementFinder> {
-        return element(by.cssContainingText(await this.sortingOptions, text))
+    getSortingOptionLocator(text: string): ElementFinder {
+        return element(by.cssContainingText(this.sortingOptions, text))
     }
     async clickSortingOption(option: string): Promise<void> {
         (await this.getSortingOptionLocator(option)).click()
